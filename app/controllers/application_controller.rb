99c313897ba803d7d093421347b99d964b87a74a
nouvelle <nouvellenuts@gmail.com>
2022-08-17 22:45:47 +0900

    feat: ログアウトボタン追加, ログイン後のページ先URLの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # ログイン後に遷移するURL
+  def after_sign_in_path_for(resource)
+    '/books'
+  end
+
+  # ログアウト後に遷移するURL
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/session#destroy'
+  end
+
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
