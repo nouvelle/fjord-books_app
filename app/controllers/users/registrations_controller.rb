960a8661b324f3a6053af329ff14e9ff3fb91668
nouvelle <nouvellenuts@gmail.com>
2022-08-18 21:47:55 +0900

    feat: ユーザ情報編集後に遷移するURLを指定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  
+  # ユーザ情報編集後に遷移するURL
+  def after_update_path_for(resource)
+    '/books'
+  end
+  
   # GET /resource/sign_up
   # def new
   #   super
