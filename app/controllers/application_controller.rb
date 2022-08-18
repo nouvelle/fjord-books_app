50a2f3ed0f5cb769d3ef025eda5ff04f4680bdf0
nouvelle <nouvellenuts@gmail.com>
2022-08-18 21:40:50 +0900

    feat: アカウント登録時・修正時に郵便番号・住所・自己紹介文を追加、修正できるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # ログイン後に遷移するURL
   def after_sign_in_path_for(resource)
     '/books'
@@ -10,5 +12,12 @@ class ApplicationController < ActionController::Base
   # ログアウト後に遷移するURL
   def after_sign_out_path_for(resource)
     '/users/sign_in'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:post_code, :address, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:post_code, :address, :introduction])
   end
 end
