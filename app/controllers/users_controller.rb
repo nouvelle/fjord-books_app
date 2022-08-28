b44c77aacc93aead6cc13a75c04d36dcaa4919de
nouvelle <nouvellenuts@gmail.com>
2022-08-28 17:44:45 +0900

    feat: フォロー数・フォロワー数の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @following = UserFollow.where(user_id: params[:id])
+    @follower = UserFollow.where(follow_id: params[:id])
+    [@user, @following, @follower]
   end
 end
