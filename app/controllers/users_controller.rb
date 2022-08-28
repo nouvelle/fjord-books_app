71674a97247f572e13f6570254584babaf4c3f55
nouvelle <nouvellenuts@gmail.com>
2022-08-28 18:44:51 +0900

    feat: フォロー一覧画面・フォロワー一覧画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,30 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @following = UserFollow.where(user_id: params[:id])
-    @follower = UserFollow.where(follow_id: params[:id])
-    [@user, @following, @follower]
+    @followings = UserFollow.where(user_id: params[:id])
+    @followers = UserFollow.where(follow_id: params[:id])
+    [@user, @followings, @followers]
+  end
+
+  def followings
+    followings = UserFollow.where(user_id: params[:user_id])
+    follow_ids = []
+    followings.each do |following|
+      follow_ids.push(following.follow_id)
+    end
+    @users = User.where("id IN (?)", follow_ids).with_attached_avatar
+    @user_id = params[:user_id]
+    [@users, @user_id]
+  end
+
+  def followers
+    followers = UserFollow.where(follow_id: params[:user_id])
+    follower_ids = []
+    followers.each do |follower|
+      follower_ids.push(follower.user_id)
+    end
+    @users = User.where("id IN (?)", follower_ids).with_attached_avatar
+    @user_id = params[:user_id]
+    [@users, @user_id]
   end
 end
