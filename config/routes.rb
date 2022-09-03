cb29935a43964cc0b45bd57d7dbd3bbd6a4666c2
nouvelle <nouvellenuts@gmail.com>
2022-09-03 16:41:05 +0900

    feat: add reports CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
+  resources :reports
   resources :books
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
