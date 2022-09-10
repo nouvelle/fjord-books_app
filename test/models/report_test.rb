c472443fd18c7b7b17d792e818141d3da55b6ceb
nouvelle <nouvellenuts@gmail.com>
2022-09-10 12:07:28 +0900

    refactor: fixturesを使用するテストにリファクタ(report)

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,27 +4,27 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable?: 本人が作成したレポートであれば true, 別のユーザが作成していれば false を返す' do
-    # me と you のアカウントを作成
-    me = User.create!(email: 'me@email.com', password: 'password')
-    you = User.create!(email: 'you@email.com', password: 'password')
+    # alice と bob のアカウントを作成
+    alice = users(:alice)
+    bob = users(:bob)
 
-    # me が report を作成
-    my_report = me.reports.new(user: me, title: 'Title', content: 'Hello World!')
+    # alice が report を作成
+    alice_report = reports(:report_alice)
     
-    # me が 作成した report であることをチェック
-    assert my_report.editable?(me)
-    # you が 作成した report でないことをチェック
-    assert_not my_report.editable?(you)
+    # alice が 作成した report であることをチェック
+    assert alice_report.editable?(alice)
+    # bob が 作成した report でないことをチェック
+    assert_not alice_report.editable?(bob)
   end
 
   test '#created_on: レポートの作成日を返す' do
-    # me のアカウントを作成
-    me = User.create!(email: 'me@email.com', password: 'password')
+    # alice のアカウントを作成
+    alice = users(:alice)
 
-    # me が report を作成
-    my_report = me.reports.new(user: me, title: 'Title', content: 'Hello World!', created_at: Date.today)
+    # alice が report を作成
+    alice_report = reports(:report_alice)
 
-    # me が 作成した report のレポート作成日が今日であることをチェック
-    assert Date.today, my_report.created_on
+    # alice が 作成した report のレポート作成日が今日であることをチェック
+    assert Date.today, alice_report.created_on
   end
 end
