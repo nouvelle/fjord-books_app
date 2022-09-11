f36d4e158b7032d35252787d00ab8202f46bba25
nouvelle <nouvellenuts@gmail.com>
2022-09-11 15:44:25 +0900

    fix: 誤記修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,7 +22,7 @@ class CommentsController < ApplicationController
   end
 
   def update
-    if @comment.update(report_params)
+    if @comment.update(comment_params)
       redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
     else
       render :edit
