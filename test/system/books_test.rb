0b666aff89d9350720623deea11174e2a88a0982
nouvelle <nouvellenuts@gmail.com>
2022-09-16 20:26:15 +0900

    fix: 不要なテストを削除・チェック項目を追加

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,7 +4,6 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:coffee)
     # トップページにアクセスする
     visit root_url
     # email アドレスを入力する
@@ -18,38 +17,60 @@ class BooksTest < ApplicationSystemTestCase
   test 'visiting the index' do
     visit books_url
     assert_selector 'h1', text: '本'
+    assert_text 'coffee'
+    assert_text '美味しいコーヒーの淹れ方'
   end
 
   test 'creating a Book' do
     visit books_url
     click_on '新規作成'
 
-    fill_in 'メモ', with: @book.memo
-    fill_in 'タイトル', with: @book.title
+    fill_in 'タイトル', with: 'タイトルテスト'
+    fill_in 'メモ', with: 'メモ欄テスト'
     click_on '登録する'
 
     assert_text '本が作成されました。'
-    click_on '戻る'
+    assert_text 'タイトルテスト'
+    assert_text 'メモ欄テスト'
   end
 
   test 'updating a Book' do
     visit books_url
+    assert_no_text 'tea'
+
     click_on '編集', match: :prefer_exact
 
-    fill_in 'メモ', with: @book.memo
-    fill_in 'タイトル', with: @book.title
+    fill_in 'タイトル', with: 'tea'
+    fill_in 'メモ', with: '美味しい紅茶の淹れ方'
     click_on '更新する'
 
     assert_text '本が更新されました。'
     click_on '戻る'
+
+    assert_selector 'h1', text: '本'
+    assert_text 'tea'
+    assert_text '美味しい紅茶の淹れ方'
   end
 
   test 'destroying a Book' do
     visit books_url
+
+    assert_text 'coffee'
+    assert_text '美味しいコーヒーの淹れ方'
+
     page.accept_confirm do
       click_on '削除', match: :first
     end
 
     assert_text '本が削除されました。'
+    assert_no_text 'coffee'
+    assert_no_text '美味しいコーヒーの淹れ方'
+  end
+
+  test 'back link from book create page' do
+    visit books_url
+    click_on '新規作成'
+    click_on '戻る'
+    assert_selector 'h1', text: '本'
   end
 end
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,7 +4,6 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:report_alice)
     # トップページにアクセスする
     visit root_url
     # email アドレスを入力する
@@ -18,38 +17,57 @@ class ReportsTest < ApplicationSystemTestCase
   test 'visiting the index' do
     visit reports_url
     assert_selector 'h1', text: '日報'
+    assert_text 'こんにちは'
+    assert_text 'alice@example.com'
   end
 
   test 'creating a Report' do
     visit reports_url
     click_on '新規作成'
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: 'タイトルテスト'
+    fill_in '内容', with: '内容テスト'
     click_on '登録する'
 
     assert_text '日報が作成されました。'
-    click_on '戻る'
+    assert_text 'タイトルテスト'
+    assert_text '内容テスト'
   end
 
   test 'updating a Report' do
     visit reports_url
+    assert_no_text 'こんばんは'
+
     click_on '編集', match: :prefer_exact
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: 'こんばんは'
+    fill_in '内容', with: 'Good Evening from Alice!'
     click_on '更新する'
 
     assert_text '日報が更新されました。'
     click_on '戻る'
+
+    assert_selector 'h1', text: '日報'
+    assert_text 'こんばんは'
   end
 
   test 'destroying a Report' do
     visit reports_url
+
+    assert_text 'こんにちは'
+
     page.accept_confirm do
       click_on '削除', match: :first
     end
 
     assert_text '日報が削除されました。'
+    assert_no_text 'こんにちは'
+  end
+
+  test 'back link from report create page' do
+    visit reports_url
+    click_on '新規作成'
+    click_on '戻る'
+    assert_selector 'h1', text: '日報'
   end
 end
